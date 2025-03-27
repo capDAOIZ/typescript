a651a491d0a441e12a838811c80aab09ccd8244c
Jorge Zavaleta <jorge9luis83@gmail.com>
2025-03-27 22:26:55 +0100

    Empezando a consumir la Api Post

--- src/components/Adoptables.tsx
+++ src/components/Adoptables.tsx
@@ -1,4 +1,40 @@
+import { useState, useEffect } from "react";
+import { getPosts } from "../services/ApiPost";
+
+interface Post {
+  id: number;
+  nameAnimal: string;
+  typeAnimal: string;
+  description: string;
+  image: File;
+}
+
 export default function Adoptables() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [error, setError] = useState("");
+  const [paginaActual, setPaginaActual] = useState(1);
+  const [totalPaginas, setTotalPaginas] = useState(1);
+
+  async function fetchPosts(page: number) {
+    try {
+      const response = await getPosts(page);
+      const data = response.posts.data;
+      setPosts(data);
+      setPaginaActual(response.posts.current_page);
+      setTotalPaginas(response.posts.last_page);
+    } catch (e: any) {
+      setError("Error al obtener los posts");
+    }
+  }
+
+  if (error) {
+    return console.error(error);
+  }
+
+  useEffect(() => {
+    fetchPosts(paginaActual);
+  }, [paginaActual]);
+
   return (
     <section className="w-full min-h-screen py-12 bg-gray-100 text-center px-12">
       <h2 className="text-3xl font-bold text-gray-800">Mascotas en Adopción</h2>
@@ -6,33 +42,33 @@ export default function Adoptables() {
         Aquí tienes algunas de las últimas mascotas en adopción.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 max-w-7xl mx-auto">
-        <div className="bg-white p-4 shadow-lg rounded-lg">
-          <img
-            src="https://source.unsplash.com/400x300/?dog"
-            alt="Perro"
-            className="w-full rounded-lg"
-          />
-          <h3 className="text-xl font-semibold mt-4">Firulais</h3>
-          <p className="text-gray-600">Juguetón y cariñoso.</p>
-        </div>
-        <div className="bg-white p-4 shadow-lg rounded-lg">
-          <img
-            src="https://source.unsplash.com/400x300/?cat"
-            alt="Gato"
-            className="w-full rounded-lg"
-          />
-          <h3 className="text-xl font-semibold mt-4">Mishi</h3>
-          <p className="text-gray-600">Tranquilo y amoroso.</p>
-        </div>
-        <div className="bg-white p-4 shadow-lg rounded-lg">
-          <img
-            src="https://source.unsplash.com/400x300/?rabbit"
-            alt="Conejo"
-            className="w-full rounded-lg"
-          />
-          <h3 className="text-xl font-semibold mt-4">Bunny</h3>
-          <p className="text-gray-600">Pequeño y esponjoso.</p>
-        </div>
+        {posts.map((post) => (
+          <div className="bg-white p-4 shadow-lg rounded-lg" key={post.id}>
+            <img alt={post.nameAnimal} />
+            <h3 className="text-xl font-bold mt-4">{post.nameAnimal}</h3>
+            <p className="text-gray-600">{post.typeAnimal}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-6 flex justify-center">
+        <button
+          onClick={() => setPaginaActual(paginaActual - 1)}
+          disabled={paginaActual === 1}
+          className="px-4 py-2 bg-gray-300 text-white rounded-l-md"
+        >
+          Anterior
+        </button>
+        <span className="px-4 py-2 flex items-center">
+          Página {paginaActual} de {totalPaginas}
+        </span>
+        <button
+          onClick={() => setPaginaActual(paginaActual + 1)}
+          disabled={paginaActual === totalPaginas}
+          className="px-4 py-2 bg-gray-300 text-white rounded-r-md"
+        >
+          Siguiente
+        </button>
       </div>
     </section>
   );
---
+++ src/services/ApiPost.tsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+const API_URL = "http://localhost/ApiPost_PF/public/api";
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+interface Post {
+  nameAnimal: string;
+  typeAnimal: string;
+  description: string;
+  image: File;
+}
+export async function getPosts(page: number = 1) {
+  try {
+    const response = await api.get(`/posts?page=${page}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener los posts", error);
+    throw error;
+  }
+}
+
+export async function getPost(id: number) {
+  try {
+    const response = await api.get(`/posts/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener el post", error);
+    throw error;
+  }
+}
+
+export async function createPost(post: Post) {
+  try {
+    const response = await api.post("/posts", post);
+
+    return response.data;
+  } catch (error) {
+    console.error("Error al crear el post", error);
+    throw error;
+  }
+}
+
+export async function updatePost(id: number, post: Post) {
+  try {
+    const response = await api.patch(`/posts/${id}`, post);
+    return response.data;
+  } catch (error) {
+    console.error("Error al actualizar el post", error);
+    throw error;
+  }
+}
+
+export async function deletePost(id: number) {
+  try {
+    const response = await api.delete(`/posts/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al eliminar el post", error);
+    throw error;
+  }
+}
